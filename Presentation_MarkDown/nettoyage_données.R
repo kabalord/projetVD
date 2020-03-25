639da65883c08ea9b7d3bcc4cfaf6820669e2487
Brice-Kevin <bricekevin55@hotmail.com>
2020-03-25 21:56:11 +0100

    Markdown : V

--- Presentation_MarkDown/nettoyage_données.R
+++ Presentation_MarkDown/nettoyage_données.R
@@ -5,7 +5,7 @@ library(car)
 
 setwd("C:/Users/HP/Desktop/Visualisation/Data")
 Football <- read.csv2("soccer-players-statistics.csv", sep = ",")
-data.Football <- as.tibble(Football[1:300,-c(3:4,7:9,14,16:17)])
+data.Football <- as.tibble(Football[1:150,-c(3:4,7:9,14,16:17)])
 
 colnames(data.Football) <- c("Nom","Nationalite","Club","Position","Evaluation",
                         "Taille_en_cm","Poids_en_kg","Pied_Fort","Age","Pied_Faible",
@@ -16,10 +16,12 @@ colnames(data.Football) <- c("Nom","Nationalite","Club","Position","Evaluation",
                         "Finition","Frappe_de_Loin","Curve","Precision_du_Coup_Franc","Penalty","Volley","Positionnement_GK",
                         "Plongee_GK","Coup_de_pied_GK","Jeu_de_Main_GK","Reflexes_GK")
 
+
 #
 data.Football$Nationalite <- as.factor(str_to_upper(data.Football$Nationalite))
 data.Football$Club <- as.factor(str_to_upper(data.Football$Club))
-data.Football$Pied_Fort <- as.factor(str_to_upper(data.Football$Pied_Fort))
+data.Football$Pied_Faible <- as.character(str_to_upper(data.Football$Pied_Faible))
+data.Football$Pied_Faible <- as.factor(str_to_upper(data.Football$Pied_Faible))
 data.Football$Nom <- str_to_upper(data.Football$Nom)
 data.Football$Taille_en_cm <- as.integer(gsub("[^0-9{,}]","",data.Football$Taille_en_cm))
 data.Football$Poids_en_kg <- as.integer(gsub("[^0-9{,}]","",data.Football$Poids_en_kg))
@@ -55,6 +57,11 @@ data.Football$Position[data.Football$Position == 'CB'] <- 'DEFENSEUR CENTRE'
 data.Football$Position[data.Football$Position == 'GK'] <- 'GARDIEN'
 
 #
+data.Football$Pied_Fort <- as.character(str_to_upper(data.Football$Pied_Fort))
+data.Football$Pied_Fort[data.Football$Pied_Fort == "RIGHT"] <- "DROIT"
+data.Football$Pied_Fort[data.Football$Pied_Fort == "LEFT"] <- "GAUCHE"
+data.Football$Pied_Fort <- as.factor(str_to_upper(data.Football$Pied_Fort))
+
 res <- filter(data.Football,Position == "RES")
 data.Football$Position[data.Football$Nom == "MATTIA PERIN"] <- "GARDIEN"
 data.Football$Position[data.Football$Nom == "GIACOMO BONAVENTURA"] <- "MILIEUX CENTRAL"
@@ -81,170 +88,69 @@ data.Football$Position <- as.factor(str_to_upper(data.Football$Position))
 #
 str(data.Football)
 summary(data.Football)
-write.table(data.Football, "../data_football.csv", row.names=FALSE, sep=",")
+
+#
+data.Football$Competence_Gardien <- as.integer(rowMeans(data.Football[,c("Plongee_GK",
+                                                                         "Coup_de_pied_GK",
+                                                                         "Jeu_de_Main_GK",
+                                                                         "Reflexes_GK",
+                                                                         "Positionnement_GK")]))
+data.Football$Plongee_GK <- data.Football$Coup_de_pied_GK <- 
+  data.Football$Jeu_de_Main_GK <- data.Football$Reflexes_GK <-
+  data.Football$Positionnement_GK <- NULL
+
+data.Football$Competence_Defense <- as.integer(rowMeans(data.Football[,c("Interception",
+                                                                         "Marquage",
+                                                                         "Tacle_Glisse",
+                                                                         "Tacle_Debout",
+                                                                         "Force",
+                                                                         "Agressivite")]))
+data.Football$Marquage <- data.Football$Tacle_Glisse <- 
+  data.Football$Tacle_Debout <- data.Football$Interception <- data.Football$Force <-
+  data.Football$Agressivite <- NULL
+
+data.Football$Competence_Milieux <- as.integer(rowMeans(data.Football[,c("Frappe_de_Loin",
+                                                                         "Endurance",
+                                                                         "Controle_de_balle",
+                                                                         "Dribbles",
+                                                                         "Calme",
+                                                                         "Passe_Courte",
+                                                                         "Passe_Longue",
+                                                                         "Vision_du_Jeu",
+                                                                         "Equilibre",
+                                                                         "Precision_du_Coup_Franc")]))
+data.Football$Frappe_de_Loin <- data.Football$Endurance <- data.Football$Controle_de_balle <-
+  data.Football$Dribbles <- data.Football$Calme <- 
+  data.Football$Passe_Courte <- data.Football$Passe_Longue <-
+  data.Football$Vision_du_Jeu <- data.Football$Equilibre <-
+  data.Football$Precision_du_Coup_Franc <- NULL
+
+data.Football$Competence_Attaque <- as.integer(rowMeans(data.Football[,c("Volley",
+                                                                         "Curve",
+                                                                         "Finition",
+                                                                         "Puissance_de_Frappe",
+                                                                         "Tete",
+                                                                         "Jump",
+                                                                         "Agilite",
+                                                                         "Vitesse",
+                                                                         "Acceleration",
+                                                                         "Geste_Technique",
+                                                                         "Attaque",
+                                                                         "Reaction",
+                                                                         "Centre",
+                                                                         "Penalty")]))
+data.Football$Volley <- data.Football$Curve <- 
+  data.Football$Finition <- data.Football$Puissance_de_Frappe <-
+  data.Football$Tete <- data.Football$Jump <- 
+  data.Football$Agilite <- data.Football$Vitesse <-
+  data.Football$Acceleration <- data.Football$Geste_Technique <- 
+  data.Football$Attaque <- data.Football$Reaction <-
+  data.Football$Centre <- data.Football$Penalty <- NULL
+
+write.table(data.Football, "../projetVD/Presentation_MarkDown/data_football.csv", row.names=FALSE, sep=",")
 
 #Clubs <- unique(data.Football[1:634,"Club"])
 #Clubs <- Clubs[-c(18,21:23,25:28,32,34:38,40,42:50,52,55:83,86:108),]
 #sink(clubs)
 #vide <- filter(data.Football,Position == "")
 
-#----------------------------------------------------------------------------------------------------------------#
-ggplot(data.Football) + 
-  geom_point(aes(x = Dribbles, y = Finition, color = Attaque))
-
-ggplot(data.Football) + 
-  geom_point(aes(x = Finition, y = Attaque, color = Vitesse)) + 
-  scale_color_gradient("Taux de joueur rapide", low = "blue", high = "green")
-
-ggplot(data.Football) + 
-  geom_point(aes(x = Finition, y = Attaque, color = Vitesse)) + 
-  scale_color_viridis("Taux de joueur rapide", option = "plasma")
-
-ggplot(data.Football) + 
-  geom_point(aes(x = Finition, y = Attaque, color = Vitesse)) + 
-  scale_color_distiller("Taux de joueur rapide", palette = "Spectral")
-
-ggplot(data.Football) + 
-  geom_point(aes(x = Finition, y = Attaque, color = Position))
-
-ggplot(data.Football) + 
-  geom_point(aes(x = Finition, y = Attaque, color = Pied_Fort)) +
-  scale_color_manual("Pied_Fort", 
-                     values = c("red", "#FFDD45", rgb(0.1,0.2,0.6), "darkgreen", "grey80"))
-
-ggplot(data.Football) + 
-  geom_point(aes(x = Finition, y = Attaque, color = Position)) +
-  scale_color_brewer("Position", palette = "Paired")
-
-ggplot(data.Football) + 
-  geom_histogram(aes(x = Tacle_Debout)) + 
-  ggtitle("Titre") +
-  xlab("Pourcentage de teacle debout") +
-  ylab("Effectif")
-
-ggplot(data.Football) +
-  aes(x = Position, y = Evaluation) +
-  geom_boxplot(fill = "#0c4c8a") +
-  theme_minimal()
-
-ggplot(data.Football) +
-  aes(x = Age, y = Evaluation) +
-  geom_point() +
-  xlab("Âge") +
-  ylab("Evaluation")
-
-ggplot(data.Football) +
-  aes(x = Evaluation) +
-  geom_histogram(fill ="orange", colour = "black") + #binwidth = 2
-  geom_rug() +
-  ggtitle("Titre") +
-  xlab("Evaluation") +
-  ylab("Effectifs")
-
-ggplot(data.Football) +
-  aes(x = Evaluation) +
-  geom_density(adjust = 1.5) +
-  ggtitle("Titre") +
-  xlab("Attaque") +
-  ylab("Densité")
-
-ggplot(data.Football) +
-  aes(x = Age, y = Position) +
-  geom_boxplot() +
-  xlab("Evaluation") +
-  ylab("Âge") +
-  ggtitle("Répartition par âge selon le poste")
-
-ggplot(data.Football) +
-  aes(x = Position, y = Evaluation) +
-  geom_boxplot() +
-  xlab("Position") +
-  ylab("Evaluation") +
-  ggtitle("Répartition par âge selon l'evaluation")
-
-ggplot(data.Football) +
-  aes(x = Position) +
-  geom_bar() +
-  xlab("Position") +
-  ylab("Effectifs")
-
-ggplot(data.Football) +
-  aes(x = Passe_Courte, y = Passe_Longue) +
-  geom_smooth(method = "lm") +
-  geom_point() +
-  xlab("Position") +
-  ylab("Passe longue")
-
-ggpairs(data.Football[, c("Passe_Courte", "Passe_Longue", "Controle_de_balle", "Pied_Fort")], aes(colour = Pied_Fort))
-
-ggcorr(data.Football)
-
-
-#ggplot(data.Football) + geom_label(aes(x = Tacle_Debout, y = Agressivite, label = Nom))
-#library(leaflet)
-#leaflet(data = data.Football) %>%
-#  addTiles %>%
-#  addCircles(lng=~lon, lat=~lat, radius=~n * 10, popup=~Nom)
-#RColorBrewer::display.brewer.all()
-#corrgram(data.Football, lower.panel=panel.cor,upper.panel=panel.pie, text.panel=panel.txt)
-#boxplot(Evaluation~Nationalite,data=data.Football,xlab="Poste",ylab="Évaluation")
-#ggplot(data = data.Football) + 
-#geom_histogram(aes(x = Evaluation)) + 
-#facet_grid(Position~Attaque)
-#ggplot(data = data.Football) + 
-#geom_histogram(aes(x = Evaluation)) + 
-#facet_grid(.~Position)
-#ggplot(data.Football) + geom_bar(aes(x = Position, fill = Attaque))
-#ggplot(data.Football) + geom_line(aes(x = Pied_Fort, y = Evaluation))
-#ggplot(data.Football) + geom_density(aes(x = Tacle_Debout, bw = 1))
-#ggplot(data.Football) + geom_density(aes(x = Tacle_Debout))
-#scatterplot(Evaluation~Age, data=data.Football,xlab="", ylab="Évaluation", main="Évaluation Vs. Age",col="red")
-#scatterplot(Tacle_Debout~Passe_Longue, data=data.Football,xlab="", ylab="Tacle debout", main="Tacle debout Vs. Passe longue")
-
-#ggplot(data.Football) + 
-#  geom_point(aes(x = Attaque, y = Dribbles, color = Position, label = Nom))#alpha = Endurance, size = Vitesse
-
-#ggplot(data.Football) + 
-#  geom_boxplot(aes(x = Position, y = Attaque)) + 
-#  geom_point(aes(x = Position, y = Attaque), col = "red", alpha = 0.2)
-
-#ggplot(data.Football,aes(x = Position, y = Attaque)) + 
-#  geom_boxplot() + 
-#  geom_jitter(col = "red", alpha = 0.2)
-
-#ggplot(data.Football, aes(x = Finition, y = Attaque)) + 
-#  geom_point(alpha = 0.2,col = "red") + 
-#  geom_density2d(color = "red") + 
-#  geom_smooth(method = "lm")
-
-#c("Nom","Nationalite","Club","Position","Evaluation",
-#  "Taille_en_cm","Poids_en_kg","Pied_Fort","Age","Pied_Faible",
-#  "Geste_Technique","Controle_de_balle","Dribbles","Marquage","Tacle_Glisse",
-#  "Tacle_Debout","Agressivite","Reaction","Attaque","Interception",
-#  "Vision_du_Jeu","Calme","Centre","Passe_Courte","Passe_Longue","Acceleration",
-#  "Vitesse","Endurance","Force","Equilibre","Agilite","Jump","Tete","Puissance_de_Frappe",
-#  "Finition","Frappe_de_Loin","Curve","Précision_du_Coup_Franc","Penalty","Volley","Positionnement_GK",
-#  "Plongee_GK","Coup_de_pied_GK","Jeu_de_Main_GK","Reflexes_GK")
-
-#filter(data.Football[,1:4],Position == "GARDIEN")
-#filter(data.Football[,1:4],Position == c("DEFENSEUR GAUCHE","DEFENSEUR DROIT","DEFENSEUR CENTRE"))
-#filter(data.Football[,1:4],Position == c("MILIEUX OFFENSIF","MILIEUX GAUCHE","MILIEUX DROIT","MILIEUX CENTRAL","MILIEUX DEFENSIF"))
-#filter(data.Football[,1:4],Position == "ATTAQUANT")
-
-#x <- ggplot(data.Football) +
-#  aes(x = Passe_Longue, fill = Position) +
-#  geom_bar() +
-#  geom_text(aes(label = ..count..), stat = "count", position = position_stack(.5)) +
-#  xlab("Passe_Longue") +
-#  ylab("Effectifs") +
-#  labs(fill = "Position")
-
-#ggsave("mon_graphique.pdf", plot = x,
-#       width = 11, height = 8)
-
-#ggplot(data.Football, aes("", fill = factor(Pied_Fort))) + 
-#  geom_bar(aes(y = (..count..)/sum(..count..)), width = 1) +
-#  scale_y_continuous() +
-#  ylab("") + xlab("") + labs(fill = "Pied_Fort") +
-#  theme(axis.ticks = element_blank()) + 
-#  coord_polar(theta = "y")
--- Presentation_MarkDown/projet_visualisation.R
+++ Presentation_MarkDown/projet_visualisation.R
@@ -7,46 +7,52 @@ library(GGally)
 
 setwd("C:/Users/HP/Desktop/Visualisation/ProjetVD/Presentation_MarkDown")
 data.Football <- read.csv2("data_football.csv", sep = ",")
+str(data.Football)
 
-ggplot(filter(data.Football,Position == "ATTAQUANT")) + geom_text(aes(x = Finition, y = Attaque, label = Nom), 
-                                                                  color = "darkred", size = 2)
+#ggpairs(data.Football[, c("Competence_Gardien","Competence_Defense","Competence_Milieux","Competence_Attaque")], aes(color = data.Football$Pied_Fort))
+
+colnames(data.Football)
 
 
+#c("Nom","Nationalite","Club","Position","Evaluation",
+#  "Taille_en_cm","Poids_en_kg","Pied_Fort","Age","Pied_Faible",
+#  "Geste_Technique","Controle_de_balle","Dribbles","Marquage","Tacle_Glisse",
+#  "Tacle_Debout","Agressivite","Reaction","Attaque","Interception",
+#  "Vision_du_Jeu","Calme","Centre","Passe_Courte","Passe_Longue","Acceleration",
+#  "Vitesse","Endurance","Force","Equilibre","Agilite","Jump","Tete","Puissance_de_Frappe",
+#  "Finition","Frappe_de_Loin","Curve","Précision_du_Coup_Franc","Penalty","Volley","Positionnement_GK",
+#  "Plongee_GK","Coup_de_pied_GK","Jeu_de_Main_GK","Reflexes_GK")
 
+#filter(data.Football[,1:4],Position == "GARDIEN")
+#filter(data.Football[,1:4],Position == c("DEFENSEUR GAUCHE","DEFENSEUR DROIT","DEFENSEUR CENTRE"))
+#filter(data.Football[,1:4],Position == c("MILIEUX OFFENSIF","MILIEUX GAUCHE","MILIEUX DROIT","MILIEUX CENTRAL","MILIEUX DEFENSIF"))
+#filter(data.Football[,1:4],Position == "ATTAQUANT")
 
-remplacer.id.nom <- function(data,x){
-  for (i in x) {
-    
-  }
+#x <- ggplot(data.Football) +
+#  aes(x = Passe_Longue, fill = Position) +
+#  geom_bar() +
+#  geom_text(aes(label = ..count..), stat = "count", position = position_stack(.5)) +
+#  xlab("Passe_Longue") +
+#  ylab("Effectifs") +
+#  labs(fill = "Position")
+
+#ggsave("mon_graphique.pdf", plot = x,
+#       width = 11, height = 8)
+
+Mat <- data.Football[,c("Nom","Evaluation","Position")]
+Mat$Equipe <- sample(1:dim(Mat)[1],150)
+GARDIEN_Equipe <- filter(Mat,Position == "GARDIEN")
+DEFENSEUR_Equipe <- filter(Mat,Position == c("DEFENSEUR GAUCHE") | Position == c("DEFENSEUR DROIT") |Position == c("DEFENSEUR CENTRE"))
+MILIEUX_Equipe <- filter(Mat,Position == c("MILIEUX OFFENSIF") | Position == c("MILIEUX GAUCHE") |Position == c("MILIEUX DROIT") | Position == c("MILIEUX CENTRAL") | Position == c("MILIEUX DEFENSIF"))
+ATTAQUANT_Equipe <- filter(Mat,Position == c("AILIER") | Position == c("ATTAQUANT"))
+
+creation.equipe <- function(G,D,M,A){
+  x <- cbind(sample(G$Nom,16),sample(D$Nom,16),sample(M$Nom,16),sample(A$Nom,16))
+  return (x);
 }
 
-creation.equipe <- function(data,n){
-  x <- c(0)
-  for (i in 1:n) {
-    y <- c(sample(1:dim(data1)[1],5))
-    x <- c(x,y)
-  }
-  x <- x[-1]
-  
-  for (i in x) {
-    for (j in 1:5) {
-      
-    }
-  }
-  return(x)
-}
 
+Mat2 <- creation.equipe(GARDIEN_Equipe,DEFENSEUR_Equipe,
+                MILIEUX_Equipe,ATTAQUANT_Equipe)
 
-data1 <- data.Football
-
-
-for (i in 2) {
-  y <- c(0)
-  x <- list(c(0))
-  for (i in 1:5) {
-    y <- c(y,sample(1:dim(data1)[1],5))
-    x <- list(x,y)
-    data1 <- data1[-c(x[[i+1]][1]),]
-  }
-}
-x
+Mat2 <- merge(Mat2,Mat,by.x="Nom",by.y="Nom")
