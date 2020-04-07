31ae785094dc8a15f56856c669a66cc16cbf0e09
Brice-Kevin <bricekevin55@hotmail.com>
2020-04-07 18:05:47 +0200

    Nettoyage et Création du fichier data_Football.csv

--- Presentation_MarkDown/nettoyage_données.R
+++ Presentation_MarkDown/nettoyage_données.R
@@ -5,17 +5,42 @@ library(car)
 
 setwd("C:/Users/HP/Desktop/Visualisation/Data")
 Football <- read.csv2("soccer-players-statistics.csv", sep = ",")
-data.Football <- as.tibble(Football[1:150,-c(3:4,7:9,14,16:17)])
 
+data.Football <- Football[1:150,-c(3:4,7:9,14,16:17,19,25:26,30,40,45:46,48)]
+#------------------------------------------------------------------------------------------------------------#
+data.Football$GK_Positioning <- as.integer(rowMeans(data.Football[,c("GK_Positioning",
+                                                                         "GK_Diving",
+                                                                         "GK_Kicking",
+                                                                         "GK_Handling",
+                                                                         "GK_Reflexes")]))
+data.Football$GK_Diving <- data.Football$GK_Kicking <- 
+  data.Football$GK_Handling <- data.Football$GK_Reflexes <- NULL
+#------------------------------------------------------------------------------------------------------------#
+data.Football$Sliding_Tackle <- as.integer(rowMeans(data.Football[,c("Marking","Sliding_Tackle","Standing_Tackle")]))
+data.Football$Standing_Tackle <- data.Football$Marking <- NULL
+#------------------------------------------------------------------------------------------------------------#
+data.Football$Acceleration <- as.integer(rowMeans(data.Football[,c("Acceleration","Speed")]))
+data.Football$Speed <- NULL
+#------------------------------------------------------------------------------------------------------------#
+data.Football$Long_Pass <- as.integer(rowMeans(data.Football[,c("Short_Pass","Long_Pass")]))
+data.Football$Short_Pass <- NULL
+#------------------------------------------------------------------------------------------------------------#
+data.Football$Dribbling <- as.integer(rowMeans(data.Football[,c("Dribbling","Ball_Control")]))
+data.Football$Ball_Control <- NULL
+#------------------------------------------------------------------------------------------------------------#
+data.Football$Attacking_Position <- as.integer(rowMeans(data.Football[,c("Attacking_Position","Finishing","Penalties")]))
+data.Football$Penalties <- data.Football$Finishing <- NULL
+#------------------------------------------------------------------------------------------------------------#
+data.Football$Shot_Power <- as.integer(rowMeans(data.Football[,c("Shot_Power","Long_Shots")]))
+data.Football$Long_Shots <- data.Football$Finishing <- NULL
+#------------------------------------------------------------------------------------------------------------#
+
+data.Football <- as.tibble(data.Football)
 colnames(data.Football) <- c("Nom","Nationalite","Club","Position","Evaluation",
                         "Taille_en_cm","Poids_en_kg","Pied_Fort","Age","Pied_Faible",
-                        "Geste_Technique","Controle_de_balle","Dribbles","Marquage","Tacle_Glisse",
-                        "Tacle_Debout","Agressivite","Reaction","Attaque","Interception",
-                        "Vision_du_Jeu","Calme","Centre","Passe_Courte","Passe_Longue","Acceleration",
-                        "Vitesse","Endurance","Force","Equilibre","Agilite","Jump","Tete","Puissance_de_Frappe",
-                        "Finition","Frappe_de_Loin","Curve","Precision_du_Coup_Franc","Penalty","Volley","Positionnement_GK",
-                        "Plongee_GK","Coup_de_pied_GK","Jeu_de_Main_GK","Reflexes_GK")
-
+                        "Dribbles","Defense","Attaque","Interception","Vision_du_Jeu",
+                        "Centre","Passe","Rapidite","Endurance","Force","Equilibre",
+                        "Agilite","Tete","Frappe","Competence_Gardien")
 
 #
 data.Football$Nationalite <- as.factor(str_to_upper(data.Football$Nationalite))
@@ -65,22 +90,19 @@ data.Football$Pied_Fort <- as.factor(str_to_upper(data.Football$Pied_Fort))
 res <- filter(data.Football,Position == "RES")
 data.Football$Position[data.Football$Nom == "MATTIA PERIN"] <- "GARDIEN"
 data.Football$Position[data.Football$Nom == "GIACOMO BONAVENTURA"] <- "MILIEUX CENTRAL"
-data.Football$Position[data.Football$Nom == "OSCAR"] <- "MILIEUX OFFENSIF"
 res <- NULL
+
 sub <- filter(data.Football,Position == "SUB")
-data.Football$Position[data.Football$Finition >= 81 & data.Football$Position == "SUB"] <- "ATTAQUANT"
-data.Football$Position[data.Football$Reflexes_GK >= 82 & data.Football$Position == "SUB"] <- "GARDIEN"
-data.Football$Position[data.Football$Acceleration >= 79 & data.Football$Dribbles >= 80 & data.Football$Position == "SUB"] <- "AILIER"
-data.Football$Position[data.Football$Nom == "SHINJI KAGAWA"] <- "MILIEUX OFFENSIF"
+data.Football$Position[data.Football$Nom == c("FRANCK RIBERY","NICOLAS GAITAN")] <- "AILIER"
+data.Football$Position[data.Football$Nom == "ARDA TURAN"] <- "AILIER"
+data.Football$Position[data.Football$Nom == "ANDRE GOMES"] <- "MILIEUX CENTRAL"
 data.Football$Position[data.Football$Nom == "DANI ALVES"] <- "DEFENSEUR DROIT"
-data.Football$Position[data.Football$Nom == "SERGE AURIER"] <- "DEFENSEUR DROIT"
-data.Football$Position[data.Football$Nom == "ALESSANDRO FLORENZI"] <- "DEFENSEUR DROIT"
-data.Football$Position[data.Football$Nom == "KAKA"] <- "MILIEUX OFFENSIF"
-data.Football$Position[data.Football$Nom == "AXEL WITSEL"] <- "MILIEUX CENTRAL"
-data.Football$Position[data.Football$Passe_Longue >= 81 & data.Football$Agilite < 86 & data.Football$Position == "SUB"] <- "MILIEUX CENTRAL"
-data.Football$Position[data.Football$Endurance >= 88  & data.Football$Endurance <= 90 & data.Football$Position == "SUB"] <- "MILIEUX CENTRAL"
-data.Football$Position[data.Football$Dribbles >= 83 & data.Football$Position == "SUB"] <- "MILIEUX OFFENSIF"
-data.Football$Position[data.Football$Marquage >= 79 & data.Football$Position == "SUB"] <- "DEFENSEUR CENTRE"
+data.Football$Position[data.Football$Nom == "CASEMIRO"] <- "MILIEUX DEFENSIF"
+data.Football$Position[data.Football$Attaque >= 72 & data.Football$Position == "SUB"] <- "MILIEUX CENTRAL"
+data.Football$Position[data.Football$Competence_Gardien >= 81 & data.Football$Position == "SUB"] <- "GARDIEN"
+data.Football$Position[data.Football$Vision_du_Jeu >= 83 & data.Football$Position == "SUB"] <- "GARDIEN"
+data.Football$Position[data.Football$Defense >= 81 & data.Football$Position == "SUB"] <- "DEFENSEUR CENTRE"
+data.Football$Position[data.Football$Dribbles >= 86 & data.Football$Position == "SUB"] <- "MILIEUX OFFENSIF"
 sub <- NULL
 
 #
@@ -89,68 +111,8 @@ data.Football$Position <- as.factor(str_to_upper(data.Football$Position))
 str(data.Football)
 summary(data.Football)
 
-#
-data.Football$Competence_Gardien <- as.integer(rowMeans(data.Football[,c("Plongee_GK",
-                                                                         "Coup_de_pied_GK",
-                                                                         "Jeu_de_Main_GK",
-                                                                         "Reflexes_GK",
-                                                                         "Positionnement_GK")]))
-data.Football$Plongee_GK <- data.Football$Coup_de_pied_GK <- 
-  data.Football$Jeu_de_Main_GK <- data.Football$Reflexes_GK <-
-  data.Football$Positionnement_GK <- NULL
-
-data.Football$Competence_Defense <- as.integer(rowMeans(data.Football[,c("Interception",
-                                                                         "Marquage",
-                                                                         "Tacle_Glisse",
-                                                                         "Tacle_Debout",
-                                                                         "Force",
-                                                                         "Agressivite")]))
-data.Football$Marquage <- data.Football$Tacle_Glisse <- 
-  data.Football$Tacle_Debout <- data.Football$Interception <- data.Football$Force <-
-  data.Football$Agressivite <- NULL
-
-data.Football$Competence_Milieux <- as.integer(rowMeans(data.Football[,c("Frappe_de_Loin",
-                                                                         "Endurance",
-                                                                         "Controle_de_balle",
-                                                                         "Dribbles",
-                                                                         "Calme",
-                                                                         "Passe_Courte",
-                                                                         "Passe_Longue",
-                                                                         "Vision_du_Jeu",
-                                                                         "Equilibre",
-                                                                         "Precision_du_Coup_Franc")]))
-data.Football$Frappe_de_Loin <- data.Football$Endurance <- data.Football$Controle_de_balle <-
-  data.Football$Dribbles <- data.Football$Calme <- 
-  data.Football$Passe_Courte <- data.Football$Passe_Longue <-
-  data.Football$Vision_du_Jeu <- data.Football$Equilibre <-
-  data.Football$Precision_du_Coup_Franc <- NULL
-
-data.Football$Competence_Attaque <- as.integer(rowMeans(data.Football[,c("Volley",
-                                                                         "Curve",
-                                                                         "Finition",
-                                                                         "Puissance_de_Frappe",
-                                                                         "Tete",
-                                                                         "Jump",
-                                                                         "Agilite",
-                                                                         "Vitesse",
-                                                                         "Acceleration",
-                                                                         "Geste_Technique",
-                                                                         "Attaque",
-                                                                         "Reaction",
-                                                                         "Centre",
-                                                                         "Penalty")]))
-data.Football$Volley <- data.Football$Curve <- 
-  data.Football$Finition <- data.Football$Puissance_de_Frappe <-
-  data.Football$Tete <- data.Football$Jump <- 
-  data.Football$Agilite <- data.Football$Vitesse <-
-  data.Football$Acceleration <- data.Football$Geste_Technique <- 
-  data.Football$Attaque <- data.Football$Reaction <-
-  data.Football$Centre <- data.Football$Penalty <- NULL
+colnames(data.Football)
 
 write.table(data.Football, "../projetVD/Presentation_MarkDown/data_football.csv", row.names=FALSE, sep=",")
 
-#Clubs <- unique(data.Football[1:634,"Club"])
-#Clubs <- Clubs[-c(18,21:23,25:28,32,34:38,40,42:50,52,55:83,86:108),]
-#sink(clubs)
-#vide <- filter(data.Football,Position == "")
 
