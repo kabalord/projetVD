5625a0359d45c55cf31aa58af0ea48f21cc094e0
kabalord <wroaserrano@gmail.com>
2020-04-15 21:33:50 +0200

    ajoute exercice en classe

--- shinyAppVD/ui.R
+++ shinyAppVD/ui.R
@@ -8,26 +8,49 @@
 #
 
 library(shiny)
+library(shinydashboard)
+data(iris)
 
+dashboardPage(
+    dashboardHeader(title = "Basic dashboard"),
+    dashboardSidebar(),
+    dashboardBody(
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            box(plotOutput("plot1", height = 250)),
+            
+            box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+            )
+        )
+    )
+)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
     titlePanel("Old Faithful Geyser Data"),
-
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput(inputId = "slide", 
+                        label = "label", 
+                        min = 1, 
+                        max = 7, 
+                        value = 4),
+            textInput(inputId = "title", 
+                      label = "title",
+                      value = "title"
+            ),
+            
+            
         ),
-
-        # Show a plot of the generated distribution
+        
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("iris.graph")
         )
     )
-))
+)
+)
