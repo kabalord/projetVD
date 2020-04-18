4d62e9817e78d2bd9a2e4ca1669b01fe7557b4f4
kabalord <wroaserrano@gmail.com>
2020-04-18 15:19:51 +0200

    publish app, affichage summary

--- shinyAppVD/server.R
+++ shinyAppVD/server.R
@@ -9,17 +9,55 @@
 
 library(shiny)
 library(dplyr)
-data(iris)
+library(dplyr)
+library(tidyverse)
+library(lattice)
+library(car)
+library(viridis)
+library(GGally)
+library(plotly)
+
+# Chargement dataset
+data.Football <- fread(file="data_football.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    
-    output$iris.graph <- renderPlot({
-        iris %>%
-            filter(Petal.Length < input$slide) %>%
-            ggplot(aes(x = Petal.Length, y = Petal.Width)) + geom_point()  +
-            labs(title = input$title)
-        
+    # Summary    
+    output$sum <- renderPrint({
+        summary(data.Football)
     })
     
+    output$head <- renderPrint({
+        head(data.Football[,1:4],10)
+    })
+    
+    # value boxes accueil
+    output$observations <- renderValueBox({
+        valueBox(
+            nrow(data.Football), icon = icon("list"), "Nombre d'observations", color = "purple")
+    })
+    
+    output$variables <- renderValueBox({
+        valueBox(
+            ncol(data.Football), icon = icon("columns"), "Nombre de variables", color = "yellow")
+    })
+    
+    output$date <- renderValueBox({
+        valueBox(2017, icon = icon("calendar"),"Date d'analyse", color = "light-blue")
+    })
+    
+    output$msgOutput <- renderMenu({
+        msgs <- apply(read.csv("messages.csv"), 1, function(row){
+            messageItem(from = row[["from"]], message = row[["message"]])
+        })
+        dropdownMenu(type = "messages", .list = msgs)
+    })
+    
+    output$performance <- renderInfoBox({
+        infoBox("Performance :", "100%", icon = icon("chart-bar"))
+    })
+    
+    output$itemRequested <- renderValueBox({
+        valueBox(15*300, "Item Request by Jouers", icon = icon("fire"), color = "yellow")
+    })
 })
--- shinyAppVD/ui.R
+++ shinyAppVD/ui.R
@@ -9,25 +9,105 @@
 
 library(shiny)
 library(shinydashboard)
-data(iris)
+library(dplyr)
+library(tidyverse)
+library(lattice)
+library(car)
+library(viridis)
+library(GGally)
+library(plotly)
+
 
 # Define UI for application that draws a histogram
 shinyUI(
-    dashboardPage(
-        dashboardHeader(title = "APP Projet Visualisation de données"),
+    dashboardPage(skin = "green",
+        dashboardHeader(title = "Football", dropdownMenuOutput("msgOutput"),
+                        # dropdownMenu(type = "message",
+                        #              # messageItem(from = "Clubs", message = "message clubs"),
+                        #              # messageItem(from = "Jouers", message = "message jouers", icon = icon("futbol"), time = "19:44"),
+                        #              # messageItem(from = "finances", message = "message finances", icon = icon("bar-chart"), time = "13-04-2020")
+                        #              # )
+                        
+                        dropdownMenu(type = "notifications",
+                                     notificationItem(
+                                         text = "2 new tabs added to the dashboard",
+                                         icon = icon("dashboard"),
+                                         status = "success"
+                                     ),
+                                     notificationItem(
+                                         text = "sever is currently running at 95% load",
+                                         icon = icon("warning"),
+                                         status = "warning"
+                                     )
+                        ),
+                        dropdownMenu(type = "tasks",
+                                     taskItem(
+                                         value = 80,
+                                         color = "aqua",
+                                         "Shiny Dasboard Education"
+                                     ),
+                                     taskItem(
+                                         value = 55,
+                                         color = "red",
+                                         "Overall R Education"
+                                     ),
+                                     taskItem(
+                                         value = 40,
+                                         color = "green",
+                                         "Data Science Education"
+                                     )
+                        )
+        ),
         dashboardSidebar(
             sidebarMenu(
-                menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-                sliderInput(inputId = "slide", label = "controls", min = 1, max = 7, value = 4)
+                sidebarSearchForm("searchText", "buttonSearch", "Search"),
+                menuItem("Accueil", tabName = "accueil", icon = icon("dashboard")),
+                menuSubItem("Clubs", tabName = "clubs"),
+                menuSubItem("Jouers", tabName ="jouers" ),
+                menuItem("Detailed Analysis", tabName = "detailed analysis", badgeLabel = "New", badgeColor = "green"),
+                menuItem("Raw Data", tabName = "row data")
             )
         ),
         dashboardBody(
-            fluidRow(
-                title = "Controls",
-                mainPanel(
-                    plotOutput("iris.graph")
+            tabItems(
+                tabItem(tabName = "accueil",
+                        fluidRow(
+                            valueBoxOutput("observations"),
+                            valueBoxOutput("variables"),
+                            valueBoxOutput("date"),
+                        ),
+                        fluidRow(
+                            box(width = 12,
+                                title = "Summary", status = "primary", solidHeader = TRUE, verbatimTextOutput("sum"), 
+                            )
+                            
+                        ),
+                ),
+                tabItem(tabName = "clubs",
+                        fluidRow(
+                            valueBox(15*200, "last match", icon = icon("hourglass-3")),
+                            valueBoxOutput("itemRequested")
+                        ),
+                        fluidRow(
+                            column(width = 12,
+                                   infoBox("Jouers force :", 1000, icon = icon("futbol")),
+                                   infoBox("Club league :", "Calcio italiano", icon = icon("trophy")),
+                                   infoBoxOutput("performance")
+                            )
+                        ),
+                        fluidRow(
+                            tabBox(
+                                tabPanel(title = "première tab", h3("contenu première tab")
+                                ),
+                                tabPanel(title = "deuxième tab", h3("contenu deuxième tab")
+                                )
+                            )
+                        )
+                ),
+                tabItem(tabName = "jouers",
+                        h1("jouers tab")
                 )
             )
-        )
+        ) 
     )
 )
