7bafcf61417453bd0dca37b4a5980718d2ba72c2
kabalord <wroaserrano@gmail.com>
2020-04-19 04:22:28 +0200

    actualisation graphs

--- shinyAppVD/server.R
+++ shinyAppVD/server.R
@@ -83,228 +83,73 @@ shinyServer(function(input, output) {
         valueBox(2017, icon = icon("calendar"),"Date d'analyse", color = "light-blue")
     })
     
-    
-    # nombre de jouers 
-    
-    output$gardiens <- renderPlot({
-        GARDIEN
-        ggplot(GARDIEN) + geom_bar(aes(x = Position),fill ="#35b779", colour = "black") + 
-            scale_x_discrete("Position") +
-            labs(x = "Position", y = "Effectif", title = "Nombre de gardiens") +
-            theme_gray()
-    })
-    
-    output$defenseurs <- renderPlot({
-        DEFENSEUR
-        ggplot(DEFENSEUR) + geom_bar(aes(x = Position),fill ="#35b779", colour = "black") + 
-            scale_x_discrete("Position") +
-            labs(x = "Position", y = "Effectif", title = "Nombre de defenseurs") +
-            theme_gray()
-    })
-    
-    output$milieux <- renderPlot({
-        MILIEUX
-        ggplot(MILIEUX) + geom_bar(aes(x = Position),fill ="#35b779", colour = "black") + 
-            scale_x_discrete("Position") +
-            labs(x = "Position", y = "Effectif", title = "Nombre de milieux") +
-            theme_gray()
-    })
-    
-    output$attaquant <- renderPlot({
-        ATTAQUANT
-        ggplot(ATTAQUANT) + geom_bar(aes(x = Position),fill ="#35b779", colour = "black") + 
-            scale_x_discrete("Position") +
-            labs(x = "Position", y = "Effectif", title = "Nombre d'attaquant") +
-            theme_gray()
-    })
-    
-    # la variable age 
-    
-    output$effectif <- renderPlot({
-        ggplot(data.Football) +
-            aes(x = Age) +
-            geom_histogram(fill ="#35b779", colour = "black",binwidth = .5) + 
-            geom_rug() +
-            ggtitle("Repartition des joueurs par evaluation") +
-            xlab("Age") +
-            ylab("Effectif") +
-            scale_y_continuous(limits = c(0,25))
-    })
-    
-    output$moyenne <- renderPlot({
-        ggplot(data.Football) + 
-            geom_boxplot(aes(x = Evaluation, y = Age), fill = "#35b779", color = "black")
-    })
-    
-    output$nuage <- renderPlot({
-        ggplot(data.Football) + 
-            geom_point(aes(x = Age, y = Evaluation), 
-                       color = "#35b779", size = 2)
-    })
-    
-    output$pointsGardians <- renderPlot({
-        ggplot(GARDIEN) + 
-            geom_point(aes(x = Age, y = Evaluation), 
-                       color = "#35b779", size = 2)
-    })
-    
-    output$textGardians <- renderPlot({
-        ggplot(GARDIEN) + 
-            geom_text(aes(x = Age, y = Evaluation, label = Nom), 
-                      color = "#35b779", size = 5)
-    })
-    
-    output$pointsDefenseurs <- renderPlot({
-        ggplot(DEFENSEUR) + 
-            geom_point(aes(x = Age, y = Evaluation), 
-                       color = "#35b779", size = 2)
-    })
-    
-    output$textDefenseurs <- renderPlot({
-        ggplot(DEFENSEUR) + 
-            geom_text(aes(x = Age, y = Evaluation, label = Nom), 
-                      color = "#35b779", size = 5)
-    })
-    
-    output$pointsMilieux <- renderPlot({
-        ggplot(MILIEUX) + 
-            geom_point(aes(x = Age, y = Evaluation), 
-                       color = "#35b779", size = 2)
-    })
-    
-    output$textMilieux <- renderPlot({
-        ggplot(MILIEUX) + 
-            geom_text(aes(x = Age, y = Evaluation, label = Nom), 
-                      color = "#35b779", size = 5)
-    })
-    
-    output$pointsAttaquants <- renderPlot({
-        ggplot(ATTAQUANT) + 
-            geom_point(aes(x = Age, y = Evaluation),
-                       color = "#35b779", size = 2)
-    })
-    
-    output$textAttaquants <- renderPlot({
-        ggplot(ATTAQUANT) + 
-            geom_text(aes(x = Age, y = Evaluation, label = Nom), 
-                      color = "#35b779", size = 5)
+    # Summary    
+    output$sum <- renderPrint({
+        summary(data.Football)
     })
     
     
-    # la variable pied fort
-    
-    output$piedMoyenne <- renderPlot({
-        ggplot(data.Football) +
-            geom_boxplot(aes(x = Pied_Fort, y = Evaluation)) + 
-            geom_jitter(aes(x = Pied_Fort, y = Evaluation), col = "#35b779", alpha = 1)
-     
+    # repartition   
+    output$repartition <- renderPlot({
+        ggplot(data = data.Football) + 
+            geom_histogram(aes(x = Evaluation),binwidth=.5,fill ="#35b779", colour = "black") + 
+            facet_wrap(~Position)
     })
     
     
-    output$attaqueGauche <- renderPlot({
+    # graphs
     
-        ggplot(data = data.Football, aes(x = Attaque, y = Evaluation)) + 
-            geom_point(alpha = 0.2) + 
-            geom_text(data = Gaucher, aes(label = Nom), color = "#35b779", size = 5)
-    })
-    
-    
-    output$attaqueDroit <- renderPlot({
-        ggplot(data = data.Football, aes(x = Attaque, y = Evaluation)) + 
-            geom_point(alpha = 0.2) + 
-            geom_text(data = Droitier, aes(label = Nom), color = "#35b779", size = 5)
-    })
-    
-    
-    output$gardienMoyenne <- renderPlot({
-        filter(GARDIEN[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(GARDIEN[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(GARDIEN) +
-            geom_boxplot(aes(x = Pied_Fort, y = Evaluation)) + 
-            labs(x = "Pied fort", y = "Evaluation", title = "Gardiens") +
-            geom_jitter(aes(x = Pied_Fort, y = Evaluation), col = "#35b779", alpha = 1) 
-    })
-    
-    output$gardienGauche <- renderPlot({
-        filter(GARDIEN[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(GARDIEN[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(data = GARDIEN, aes(x = Competence_Gardien, y = Evaluation)) + 
-            geom_point(alpha = 0.2) + 
-            labs(x = "Defense", y = "Evaluation", title = "Gardiens") +
-            geom_text(data = filter(GARDIEN,Pied_Fort == "GAUCHE"), aes(label = Nom), color = "#35b779", size = 5)
-    })
-    
-    output$gardienDroit <- renderPlot({
-        filter(GARDIEN[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(GARDIEN[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(data = GARDIEN, aes(x = Competence_Gardien, y = Evaluation)) + 
-            geom_point(alpha = 0.2) + 
-            labs(x = "Defense", y = "Evaluation", title = "Gardiens") +
-            geom_text(data = filter(GARDIEN,Pied_Fort == "DROIT"), aes(label = Nom), color = "#35b779", size = 5)
-    })
+    output$EvalPlot = renderPlot({
+            data.Football%>%filter(Evaluation <= input$Slide)%>%
+                ggplot(aes(Evaluation)) +
+                geom_histogram(fill ="#35b779", colour = "black",binwidth = .5) +
+                geom_rug() +
+                ggtitle("Repartition des joueurs par evaluation") +
+                xlab("Evaluation") +
+                ylab("Effectif") +
+                theme_light() +
+                scale_y_continuous(limits = c(0,50))
+        })
     
     
-    output$defenseurMoyenne <- renderPlot({
-        filter(DEFENSEUR[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(DEFENSEUR[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(DEFENSEUR) +
-            geom_boxplot(aes(x = Pied_Fort, y = Evaluation)) + 
-            labs(x = "Pied fort", y = "Evaluation", title = "Defenseurs") +
-            geom_jitter(aes(x = Pied_Fort, y = Evaluation), col = "#35b779", alpha = 1) 
-    })
+    output$myhist = renderPlot({
+        data.Football%>%filter(Evaluation <= input$Slide & Position == input$var)%>% 
+            ggplot(aes(Taille_en_cm, Poids_en_kg)) +
+            geom_point(aes(colour = Position))
+        })
     
-    output$defenseurpiefort <- renderPlot({
-        filter(DEFENSEUR[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(DEFENSEUR[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(data = DEFENSEUR, aes(x = Defense, y = Evaluation)) + 
-            geom_point(alpha = 0.2) + 
-            labs(x = "Defense", y = "Evaluation", title = "Defenseurs") +
-            geom_text(data = filter(DEFENSEUR,Pied_Fort == "GAUCHE"), aes(label = Nom), color = "#35b779", size = 2)
-    })
+    output$myhisto = renderPlot({
+            data.Football%>%filter(Position == input$var)%>% 
+                ggplot() + geom_bar(aes(x = Position),fill ="#35b779", colour = "black") + 
+                scale_x_discrete("Position") +
+                labs(x = "Position", y = "Effectif") +
+                theme_gray()
+        })
     
-    output$milieuxMoyenne <- renderPlot({
-        filter(MILIEUX[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(MILIEUX[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(MILIEUX) +
-            geom_boxplot(aes(x = Pied_Fort, y = Evaluation)) + 
-            labs(x = "Pied fort", y = "Evaluation", title = "Milieux") +
-            geom_jitter(aes(x = Pied_Fort, y = Evaluation), col = "#35b779", alpha = 1) 
-        
-    })
     
-    output$milieuxMoyenne <- renderPlot({
-        filter(MILIEUX[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(MILIEUX[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        geom_boxplot(aes(x = Pied_Fort, y = Evaluation)) + 
-            labs(x = "Pied fort", y = "Evaluation", title = "Milieux") +
-            geom_jitter(aes(x = Pied_Fort, y = Evaluation), col = "#35b779", alpha = 1) 
-    })
-    
-    output$milieuxpiefort <- renderPlot({
-        filter(MILIEUX[c(1,4,8)],Pied_Fort == "GAUCHE")
-        filter(MILIEUX[c(1,4,8)],Pied_Fort == "DROIT")
-        
-        ggplot(MILIEUX) +
+    output$myhist1 = renderPlot(
+        {
+            GARDIEN%>%filter(Taille_en_cm <= input$Slide & Poids_en_kg <= input$Slide2)%>% 
+                ggplot(aes(x=Taille_en_cm, y=Poids_en_kg, colour=Position, shape=Position))+
+                geom_point()
+        }
+    )
+    output$myhist2 = renderPlot({
+            DEFENSEUR%>%filter(Taille_en_cm <= input$Slide & Poids_en_kg <= input$Slide2)%>% 
+                ggplot(aes(x=Taille_en_cm, y=Poids_en_kg, colour=Position, shape=Position))+
+                geom_point()
+        })
+    output$myhist3 = renderPlot({
+            MILIEUX%>%filter(Taille_en_cm <= input$Slide & Poids_en_kg <= input$Slide2)%>% 
+                ggplot(aes(x=Taille_en_cm, y=Poids_en_kg, colour=Position, shape=Position))+
+                geom_point()
+        })
+    output$myhist4 = renderPlot({
+            ATTAQUANT%>%filter(Taille_en_cm <= input$Slide & Poids_en_kg <= input$Slide2)%>% 
+                ggplot(aes(x=Taille_en_cm, y=Poids_en_kg, colour=Position, shape=Position))+
+                geom_point()
+        })
 
-        ggplot(data = MILIEUX, aes(x = Passe, y = Evaluation)) + 
-            geom_point(alpha = 0.2) + 
-            labs(x = "Passe", y = "Evaluation", title = "Milieux") +
-            geom_text(data = filter(MILIEUX,Pied_Fort == "GAUCHE"), aes(label = Nom), color = "#35b779", size = 2)
-    })
-    
-    
-    
-    
-    
-    
-    
     
     
     
--- shinyAppVD/ui.R
+++ shinyAppVD/ui.R
@@ -17,6 +17,8 @@ library(viridis)
 library(GGally)
 library(plotly)
 library(ggcorrplot)
+x <- levels(data.Football$Position)
+
 
 # Define UI for application that draws a histogram
 shinyUI(
@@ -62,8 +64,8 @@ shinyUI(
             sidebarMenu(
                 sidebarSearchForm("searchText", "buttonSearch", "Search"),
                 menuItem("Accueil", tabName = "accueil", icon = icon("dashboard")),
-                menuItem("Age", tabName = "age", icon = icon("user-clock")),
-                menuItem("Pied fort", tabName = "Pied", icon = icon("walking"))
+                menuItem("Les tables", tabName = "tables", icon = icon("table")),
+                menuItem("Les graphes", tabName = "graphes", icon = icon("chart-bar"))
             )
         ),
         dashboardBody(
@@ -78,190 +80,56 @@ shinyUI(
                              
                         ),
                         fluidRow(
+                            box(width = 12,
+                                title = "Summary", status = "danger", solidHeader = TRUE, verbatimTextOutput("sum"),  
+                            ),
+
+                            
+                        ),
+                        fluidRow(
                             box(width = 4,
                                 title = "Correlation", status = "primary", solidHeader = TRUE, plotOutput("correlation"), 
-                                sliderInput("controlsCorrelation", "Mesure de correlation", -1, 1, 0),
                             ),
                             box(width = 8,
-                                title = "Secteurs", status = "danger", solidHeader = TRUE,
-                                tabBox(width = 12,
-                                       tabPanel(title = "Defensif", 
-                                       plotlyOutput("defensif"),
-                                       sliderInput("defensif", "Nombre d'observations", 1, 90, 45),
-                                       ),
-                                       tabPanel(title = "Offensif", 
-                                        plotlyOutput("offensif"),
-                                       )
-                                )
+                                title = "Reparition par poste", status = "warning", solidHeader = TRUE, plotOutput("repartition"),
                             )
-                            
-                        ),
-                        fluidRow(
-                            box(width = 4,
-                                title = "Repartition des jouers par evaluation", status = "warning",solidHeader = TRUE, plotOutput("evaluation"),
-                            ),
-                            box(width = 8,
-                                title = "Poste represent", status = "info",solidHeader = TRUE, plotOutput("represent"),
-                            )
-                        ),
-                        fluidRow(
-                            
-                            box(
-                                title = "Nombre de jouers", status = "primary", solidHeader = TRUE,
-                                tabBox(width = 12,
-                                       tabPanel(title = "Attaquant", 
-                                                plotOutput("attaquant"),
-                                       ),
-                                       tabPanel(title = "Defenseurs", 
-                                                plotOutput("defenseurs"),
-                                       ),
-                                       tabPanel(title = "Milieux", 
-                                                plotOutput("milieux"),
-                                       ),
-                                       tabPanel(title = "Gardiens", 
-                                                plotOutput("gardiens"),
-                                       )
-                                )
-                            ),
-                            box(
-                                title = "La variable 'Age'", status = "danger", solidHeader = TRUE,
-                                tabBox(width = 12,
-                                       tabPanel(title = "Effectif ", 
-                                                plotOutput("effectif"),
-                                       ),
-                                       tabPanel(title = "Moyenne", 
-                                                plotOutput("moyenne"),
-                                       ),
-                                       tabPanel(title = "Nuage", 
-                                                plotOutput("nuage"),
-                                       )
-                                )
-                            ),
-                            
                         )
-                ),
-                tabItem(tabName = "age",
-                    fluidRow(
-                        box(
-                            title = "Pour les gardians", status = "primary", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                tabPanel(title = "Points",
-                                         plotOutput("pointsGardians"),
-                                ),
-                                tabPanel(title = "Text",
-                                         plotOutput("textGardians"),
-                                )
-                            )
-                        ),
-                        box(
-                            title = "Pour les defenseurs", status = "danger", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Points",
-                                            plotOutput("pointsDefenseurs"),
-                                   ),
-                                   tabPanel(title = "Text",
-                                            plotOutput("textDefenseurs"),
-                                   )
-                            )
-                        ),
-                        box(
-                            title = "Pour les milieux", status = "warning", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Points",
-                                            plotOutput("pointsMilieux"),
-                                   ),
-                                   tabPanel(title = "Text",
-                                            plotOutput("textMilieux"),
-                                   )
-                            )
-                        ),
-                        box(
-                            title = "Pour les attaquants", status = "info", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Points",
-                                            plotOutput("pointsAttaquants"),
-                                   ),
-                                   tabPanel(title = "Text",
-                                            plotOutput("textAttaquants"),
-                                   )
-                            )
-                        ),
-                    )
-                ),
-                tabItem(tabName = "Pied",
-                    fluidRow(
-                        box(
-                            title = "Moyenne", status = "primary", solidHeader = TRUE,
-                            plotOutput("piedMoyenne"),
-                            sliderInput("controlsMoyenne", "Numero effectifs", 1, 150, 76),
-                        ),
-                        box(
-                            title = "Attaque", status = "danger", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Gauche",
-                                            plotOutput("attaqueGauche"),
-                                   ),
-                                   tabPanel(title = "Droit",
-                                            plotOutput("attaqueDroit"),
-                                   )
-                            )
-                            
-                        )
-                    ),
-                    fluidRow(
-                        box(
-                            title = "Gardiens", status = "warning", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Moyenne",
-                                            plotOutput("gardienMoyenne"),
-                                   ),
-                                   tabPanel(title = "Gauche",
-                                            plotOutput("gardienGauche"),
-                                   ),
-                                   tabPanel(title = "Droit",
-                                            plotOutput("gardienDroit"),
-                                   )
-                            )
-                        ),
-                        box(
-                            title = "Defenseurs", status = "info", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Moyenne",
-                                            plotOutput("defenseurMoyenne"),
-                                   ),
-                                   tabPanel(title = "Defense",
-                                            plotOutput("defenseurpiefort"),
-                                   )
-                            )
-                        ),
 
-                    ),
-                    fluidRow(
-                        box(
-                            title = "Milieux", status = "danger", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Moyenne",
-                                            plotOutput("milieuxMoyenne"),
-                                   ),
-                                   tabPanel(title = "Passe",
-                                            plotOutput("milieuxpiefort"),
-                                   )
-                            )
-                            
+ 
+                ),
+                tabItem(tabName = "tables",
+ 
+                ),
+                tabItem(tabName = "graphes",
+                        fluidRow(
+                          box(
+                              title = "primière graph", status = "primary", solidHeader = TRUE, plotOutput("EvalPlot"),
+                              sliderInput('Slide', 'Mex evaluation', min = 80, max = 100, value = 82)
+                          ),
+                          box(
+                              title = "deuxième graph", status = "primary", solidHeader = TRUE, plotOutput("myhist"),
+                              selectInput('var', "Select the Variable", choices = x, selected = 1),
+                              br(),
+                              sliderInput('Slide', 'Mex evaluation', min = 82, max = 100, value = 82),
+                          )
                         ),
-                        box(
-                            title = "Attaquant", status = "success", solidHeader = TRUE,
-                            tabBox(width = 12,
-                                   tabPanel(title = "Moyenne",
-                                            plotOutput("attaquantMoyenne"),
-                                   ),
-                                   tabPanel(title = "Attaque",
-                                            plotOutput("attaquantGauche"),
-                                   )
+                        fluidRow(
+                            box(
+                                title = "troisième graph", status = "primary", solidHeader = TRUE, plotOutput("myhisto"),
+                                selectInput('var', "Select the Variable", choices = x, selected = 1),
+                                br(),
+                                sliderInput('Slide', 'Mex evaluation', min = 82, max = 100, value = 82)
+                            ),
+                            box(
+                                title = "quatrième graph", status = "primary", solidHeader = TRUE, 
+                                plotOutput("myhist1"), 
+                                plotOutput("myhist2"),
+                                plotOutput("myhist3"),
+                                plotOutput("myhist4"),
+                                sliderInput('Slide', 'Taille', min = 160, max = 200, value = 160),
+                                sliderInput('Slide2', 'Poids', min = 50, max = 100, value = 60)
                             )
-                            
-                        ),
-                    )
+                        )
                     
                 )
             )
